db0923aa5e67066dd9ee991012de4aa52a7da247
Julio Trecenti <julio.trecenti@gmail.com>
2017-11-26 14:25:36 -0200

    checking with note

---
+++ R/d_sf.R
@@ -0,0 +1,13 @@
+#' Shapefile for jurimetric data
+#'
+#' A dataset containing shapefiles for municipalities, comarcas,
+#' circunscricoes and regions
+#'
+#' @format A data frame with 4 rows and 2 variables:
+#' \describe{
+#'   \item{nivel}{level of data (municipality, comarca, circunscricao, region)}
+#'   \item{sf}{simple feature complex data}
+#'   ...
+#' }
+#' @source \url{http://www.tjsp.jus.br/ListaTelefonica}
+"d_sf"
