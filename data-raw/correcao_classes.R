e9e2d7c401a334b2e105e3e095719e1a4373dc7c
Julio Trecenti <julio.trecenti@gmail.com>
2020-12-05 10:33:42 -0300

    correcao das classes do objeto TJRS

--- data-raw/correcao_classes.R
+++ data-raw/correcao_classes.R
@@ -6,6 +6,8 @@ d_sf_tjrs$sf <- purrr::map(d_sf_tjrs$sf, ~{
     sf::st_as_sf()
 })
 
+usethis::use_data(d_sf_tjrs, overwrite = TRUE, compress = "xz")
+
 d_sf_tjsp <- d_sf
 
 d_sf_tjsp$sf <- purrr::map(d_sf_tjsp$sf, ~{
