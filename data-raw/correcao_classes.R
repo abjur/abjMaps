7c8944ceb62ef2c0d5d213ac51ffccd1112da499
Julio Trecenti <julio.trecenti@gmail.com>
2020-12-05 10:21:51 -0300

    remove old functions from export

---
+++ data-raw/correcao_classes.R
@@ -0,0 +1,17 @@
+library(abjMaps)
+
+d_sf_tjrs$sf <- purrr::map(d_sf_tjrs$sf, ~{
+  .x %>%
+    tibble::as_tibble() %>%
+    sf::st_as_sf()
+})
+
+d_sf_tjsp <- d_sf
+
+d_sf_tjsp$sf <- purrr::map(d_sf_tjsp$sf, ~{
+  .x %>%
+    tibble::as_tibble() %>%
+    sf::st_as_sf()
+})
+
+usethis::use_data(d_sf_tjsp, compress = "xz")
